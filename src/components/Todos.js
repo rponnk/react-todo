1665fe95be17abab5e3e1e1919497133d9ef8ecb
rponnk <rponnk@users.noreply.github.com>
2019-02-04 19:52:46 -0600

    updated files

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import TodoItem from './TodoItem';
 
 class Todos extends React.Component {
     render() {
         return (
             this.props.todos.map((todo) => (
-                <h3>{todo.title}</h3>
+                <TodoItem
+                    key={todo.id}
+                    title={todo.title
+                }/>
             ))
         );
     }
